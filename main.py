61668a15b88d1151019ea441d07183694a354448
MarionSauvage <marionsauvage@outlook.com>
2021-01-11 16:29:13 +0100

    update

---
+++ main.py
@@ -0,0 +1,6 @@
+from skimage.io import imread, imshow, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+from sklearn.model_selection import train_test_split
+
+
