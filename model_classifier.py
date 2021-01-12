cda6a2c1aa100cb5b0ac4d11d6fd1612e6ce22e4
Michaël Zanatta <michael.zanatta@cegetel.net>
2021-01-12 19:19:11 +0100

    Quick classification test, poor accuracy

--- model_classifier.py
+++ model_classifier.py
@@ -9,7 +9,7 @@ class Net(Module):
 
         self.cnn_layers = Sequential(
             # Defining a 2D convolution layer
-            Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
+            Conv2d(3, 4, kernel_size=3, stride=1, padding=1),
             BatchNorm2d(4),
             ReLU(inplace=True),
             MaxPool2d(kernel_size=2, stride=2),
@@ -21,7 +21,9 @@ class Net(Module):
         )
 
         self.linear_layers = Sequential(
-            Linear(4 * 7 * 7, 10)
+            Linear(4 * 64 * 64, 10),
+            ReLU(),
+            Linear(10, 2)
         )
 
     # Defining the forward pass    
