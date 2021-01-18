8b2d39f8b1d0f8e24bb7e597ecda802ee5084ac0
MarionSauvage <marionsauvage@outlook.com>
2021-01-18 21:32:04 +0100

    model

--- model_classifier.py
+++ model_classifier.py
@@ -9,19 +9,21 @@ class Net(Module):
 
         self.cnn_layers = Sequential(
             # Defining a 2D convolution layer
-            Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
-            BatchNorm2d(4),
+            Conv2d(3, 4, kernel_size=3, stride=1, padding=1),
             ReLU(inplace=True),
             MaxPool2d(kernel_size=2, stride=2),
+            BatchNorm2d(4),
             # Defining another 2D convolution layer
             Conv2d(4, 4, kernel_size=3, stride=1, padding=1),
-            BatchNorm2d(4),
             ReLU(inplace=True),
             MaxPool2d(kernel_size=2, stride=2),
+            BatchNorm2d(4),
         )
 
         self.linear_layers = Sequential(
-            Linear(4 * 7 * 7, 10)
+            Linear(4 * 64 * 64, 10),
+            ReLU(inplace=True),
+            Linear(10, 2)
         )
 
     # Defining the forward pass    
