370f69117f98ee205a43bd59b3801d6b50270fec
MarionSauvage <marionsauvage@outlook.com>
2021-01-18 21:22:29 +0100

    functions for classification

---
+++ preprocessing_segmentation.py
@@ -0,0 +1,19 @@
+
+
+class BrainMriDataset(Dataset):
+    def __init__(self, df, transforms):
+        
+        self.df = df
+        self.transforms = transforms
+        
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, idx):
+        image = cv2.imread(self.df.iloc[idx, 1])
+        mask = cv2.imread(self.df.iloc[idx, 3], 0)
+        
+        augmented_img = self.transforms(image)
+        
+        return augmented_img, self.df.iloc[idx, 5]
+
