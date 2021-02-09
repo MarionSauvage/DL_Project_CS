c219f7e5d33d9dace45a95df8d1a23c998d736b7
MarionSauvage <marionsauvage@outlook.com>
2021-02-09 19:08:00 +0100

    iou

--- segmentation.py
+++ segmentation.py
@@ -15,49 +15,66 @@ def evaluate_model(model, device,val_loader, optimizer, criterion):
     model.eval()
     num_batches = 0
     avg_loss = 0
+    iou=0
     for idx, sample in enumerate(val_loader):
-        train_iou = []
+        
         data = sample['image']
         target = sample['mask']
-        data, target = Variable(data.type(opt.dtype)), Variable(target.type(opt.dtype))
+        data, target = data.to(device),target.to(device)
         output = model.forward(data)
         #IOU computation
         out_cut = np.copy(outputs.data.cpu().numpy())
         out_cut[np.nonzero(out_cut < 0.5)] = 0.0
         out_cut[np.nonzero(out_cut >= 0.5)] = 1.0
-        train_dice = compute_iou(out_cut, target.data.cpu().numpy())
+        val_iou = compute_iou(out_cut, target.data.cpu().numpy())
+        iou+=val_iou
         ## LOSS
         loss = criterion(output, target)
         avg_loss += loss.data[0]
         num_batches += 1
     avg_loss /= num_batches
     print('epoch: ' + str(epoch) + ', validation loss: ' + str(avg_loss))
-    return avg_loss
+    return val_iou/idx,avg_loss
 
 
 def train_segmentation(model, device, train_loader,val_loader, optimzer, criterion, epochs=20):
+    loss_history = []
+    train_history = []
+    val_history = []
     for epoch in range(epochs):
         num_batches = 0
         avg_loss = 0
-        for idx, samplein enumerate(train_loader):
+        train_iou = []
+        losses=[]
+        for idx, sample in enumerate(train_loader):
             model.train()
             optimizer.zero_grad()
             data = sample['image']
             target = sample['mask']
             data, target = data.to(device), target.to(device)
             output = model(data)
-            # output = (output > 0.5).type(opt.dtype)	# use more gpu memory, also, loss does not change if use this line
+            #IOU computation
+            out_cut = np.copy(outputs.data.cpu().numpy())
+            out_cut[np.nonzero(out_cut < 0.5)] = 0.0
+            out_cut[np.nonzero(out_cut >= 0.5)] = 1.0
+            train_dice = compute_iou(out_cut, target.data.cpu().numpy())
+            train_iou.append(train_dice)
+            ## LOSS
             loss = criterion(output, target)
+            losses.append(loss.item())
             loss.backward()
             optimizer.step()
             avg_loss += loss.data[0]
-            num_batches += 1
             if idx % 20 == 0:
                     val_loss= evaluate_model(model, device, val_loader, optimizer, criterion)
 
                     print('epoch {} batch {}  [{}/{}] training loss: {:1.4f} \tvalidation loss: {:1.4f:}'.format(epoch,idx,idx*len(x),
                             len(train_loader.dataset),loss.item(), val_loss))
-    return 
+        
+        loss_history.append(np.array(losses).mean())
+        iou_train_history.append(np.array(train_iou).mean())
+        val_loss_history.append(val_mean_iou)
+    return loss_history, iou_train_history,val_loss_history
 
 # make prediction
 def run_test(model, test_loader, opt):
