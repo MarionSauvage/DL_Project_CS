57fdfdbb2ef4f82db953e24aaf6a7b473f09104c
Michael Zanatta <michael.zanatta@cegetel.net>
2021-02-13 17:31:11 +0100

    Add accuracy early stopping

--- classification.py
+++ classification.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch.autograd import Variable
 from ray import tune
@@ -26,6 +27,11 @@ def evaluate_model(model, device, dataloader, optimizer, criterion):
 
 
 def train_classification(model, device, train_dataloader, val_dataloader, optimizer, criterion, epochs=20):
+    # Early stopping initialization
+    n_epochs_stop_acc = 5
+    epochs_no_improve_acc = 0
+    max_val_acc = 0
+
     for epoch in range(epochs):
     # training
         for batch_idx, (x, target) in enumerate(train_dataloader):
@@ -44,6 +50,24 @@ def train_classification(model, device, train_dataloader, val_dataloader, optimi
                 val_loss, accuracy = evaluate_model(model, device, val_dataloader, optimizer, criterion)
                 print('epoch {} batch {}  [{}/{}] training loss: {:1.4f} \tvalidation loss: {:1.4f}\tAccuracy (val): {:.1%}'.format(epoch,batch_idx,batch_idx*len(x),
                         len(train_dataloader.dataset),loss.item(), val_loss, accuracy))
+
+        # Get the new validation accuracy
+        val_loss, accuracy = evaluate_model(model, device, val_dataloader, optimizer, criterion)
+        print("Accuracy (val): {:.1%}".format(accuracy))
+
+        # Early stopping accuracy tracking        
+        if accuracy > max_val_acc:
+            epochs_no_improve_acc = 0
+            max_val_acc = accuracy
+        else:
+            epochs_no_improve_acc += 1
+        
+        if epoch >= 10 and epochs_no_improve_acc >= n_epochs_stop_acc:
+            print('Early stopping !')
+            print("Stopped")
+            break
+        else:
+            continue
     
     # Get the last validation accuracy
     val_loss, accuracy = evaluate_model(model, device, val_dataloader, optimizer, criterion)
