403d5c21552e1b3803399f4f6fcd6276d14da5be
MarionSauvage <marionsauvage@outlook.com>
2021-02-17 15:24:02 +0100

    unet resnet

--- model_segmentation.py
+++ model_segmentation.py
@@ -6,8 +6,9 @@ from torch.optim import Adam, SGD
 from torchvision import models
 from torchvision.models import resnext50_32x4d, resnet18
 
-backbone=resnet18(pretrained=True)
-#backbone=resnext50_32x4d(pretrained=True)
+
+resnet_backbone = resnet18(pretrained=True)
+resnext_backbone = resnext50_32x4d(pretrained=True)
 
 def DoubleConv(in_channels, out_channels):
     return nn.Sequential(
@@ -66,8 +67,57 @@ class UnetResNet(nn.Module):
         super().__init__()
 
         ## Resnet backbone 
-        self.base_model = backbone
-        self.base_layers = list(backbone.children())  
+        self.base_model = resnet_backbone
+        self.base_layers = list(resnet_backbone.children())
+
+        #Left side of UNET : Sequential NN
+        self.conv_left1 = nn.Sequential(self.base_layers[:4][0])
+        self.conv_left2 = nn.Sequential(self.base_layers[5][0])
+        self.conv_left3 = nn.Sequential(self.base_layers[6][0])
+        self.conv_left4 = nn.Sequential(self.base_layers[7][0])
+
+        self.maxpool = nn.MaxPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)        
+        
+        #Right side of UNET : 
+        self.conv_right3 = DoubleConv(256 + 512, 256)
+        self.conv_right2 = DoubleConv(128 + 256, 128)
+        self.conv_right1 = DoubleConv(128 + 64, 64)
+        
+        self.last_conv = nn.Conv2d(64, nb_classes, kernel_size=1)
+
+    def forward(self,x):
+        conv1 = self.conv_left1(x)
+        conv2 = self.conv_left2(conv1)
+        conv3 = self.conv_left3(conv2)
+        conv4 = self.conv_left4(conv3)
+        x = self.upsample(conv4)
+
+        x = torch.cat([x, conv3], dim=1)
+
+        x = self.conv_right3(x)
+        x = self.upsample(x)
+        x = torch.cat([x, conv2], dim=1)
+
+        x = self.conv_right2(x)
+        x = self.upsample(x)
+        x = torch.cat([x, conv1], dim=1)
+
+        x = self.conv_right1(x)
+        x = self.upsample(x)
+
+        out = self.last_conv(x)
+        out = torch.sigmoid(out)
+        return out
+
+
+class UnetResNext(nn.Module):
+    def __init__(self,nb_classes=1):
+        super().__init__()
+
+        ## Resnet backbone 
+        self.base_model = resnext_backbone
+        self.base_layers = list(resnext_backbone.children())
 
         #Left side of UNET : Sequential NN
         self.block_left0 = nn.Sequential(*self.base_layers[:3])
@@ -88,7 +138,6 @@ class UnetResNet(nn.Module):
         # Final Classifier
         # self.last_conv0 = ConvRelu(256, 128, 3, 1)
         # self.last_conv1 = nn.Conv2d(128, nb_classes, 3, padding=1)
-
 
     def forward(self,x):
         #ENCODER BLOCK
@@ -125,6 +174,8 @@ class UnetResNet(nn.Module):
 def build_model(nn_type='Unet'):
     if nn_type == 'UnetResNet':
         model = UnetResNet()
+    elif nn_type == 'UnetResNext':
+        model = UnetResNext()
     else:
         model = Unet()
 
